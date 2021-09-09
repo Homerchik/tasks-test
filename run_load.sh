05eba4dd2a3dd1d7f8c15a50919a7955b01e6cb9
homerchik <homerchik@gmail.com>
2021-09-09 14:55:32 +0300

    drop tank container after script finish

--- run_load.sh
+++ run_load.sh
@@ -7,5 +7,5 @@ docker load -i $IMAGE_SRC
 docker pull direvius/yandex-tank
 
 docker run -e VERBOSE=1 --name "todo"  -d --rm -p "8080:4242" $IMAGE
-docker run --name "ytank" -v "$(pwd)":/var/loadtest --net host -it direvius/yandex-tank -c configs/tank.yaml
+docker run --name "ytank" --rm -v "$(pwd)":/var/loadtest --net host -it direvius/yandex-tank -c configs/tank.yaml
 docker stop "todo"
